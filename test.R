58ffa280c564317fc90626a5c93c86ce5458eb93
Igor Prokhorov <iprokhorovster@gmail.com>
2014-07-18 16:18:25 +0400

    changes

--- test.R
+++ test.R
@@ -1,6 +1,6 @@
-####Test assignment 2
+##Test assignment 2
 source("ProgrammingAssignment2/cachematrix.R")
-#####
+##
 
 x <- matrix(rnorm (100, 1, 0.5), 10, 10)
 system.time(for(i in 1:100000) { solve(x) } )
@@ -12,7 +12,7 @@ z <- makeCacheMatrixV2(x)
 system.time(for(i in 1:100000) { cacheSolve(q) } )
 
 
-###########
+##
 # create a 10x10 matrix
 mat <- makeCacheMatrix(matrix(rnorm(100), c(10,10)))
 stopifnot(!is.null(mat$get()))
